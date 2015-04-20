9dd431ef7c4aca23fd737e014992818d6f0bd664
Gabriel Machado <gabriel_machado@live.com>
2015-04-20 18:58:29 -0300

    AST viz

--- Parser.hs
+++ Parser.hs
@@ -1,55 +1,10 @@
+module Parser where
+
 import Text.Parsec
 import Text.Parsec.String
 import Control.Applicative ((<*>),(<*),(*>),(<$>))
 
-type Nameid = String
-
-data Typeid     = Int
-                | Bool
-                | String
-                | Vector     Typeid
-   deriving Read
-
-data Program    = Program   [FuncDecl] Scope                        deriving Show
-data Scope      = Scope     [VarDecl] [Command]                     deriving Show
-
-data FuncDecl   = FuncDecl   Typeid    Nameid   [ParamDecl] Scope   deriving Show
-data ParamDecl  = ParamDecl  Typeid    Nameid                       deriving Show
-data VarDecl    = VarDecl    Typeid    Nameid   (Maybe Expr)        deriving Show
-
-data Command    = While      Expr     [Command]
-                | If         Expr     [Command]
-                | IfElse     Expr     [Command] [Command]
-                | For        Nameid    Expr      Expr      [Command]
-                | Free       Nameid
-                | Attrib     Expr      Expr
-                | Expr       Expr
-   deriving Show
-
-data Expr       = IntLit     Int
-                | BoolLit    Bool
-                | StringLit  String
-                | Var        Nameid   [Expr]
-                | Not        Expr
-                | BinOp      Expr      String    Expr  -- Operador binário associativo à esquerda
-                | Func       Nameid   [Expr]
-                | Alloc      Typeid   [Expr]
-
-instance Show Typeid where
-    show Int = "Int"
-    show Bool = "Bool"
-    show String = "String"
-    show (Vector t) = (show t) ++ "[]"
-
-instance Show Expr where
-    show (IntLit i) = show i
-    show (BoolLit i) = show i
-    show (StringLit i) = show i
-    show (BinOp a o b) = '(' : (show a) ++ o ++ (show b) ++ ")"
-    show (Var n e) = n ++ (foldl (\a x->a ++ '[' : (show x) ++ "]") "" e)
-    show (Not e) = "!(" ++ show e ++ ")"
-    show (Alloc t e) = "novo " ++ show t ++ (foldl (\a x->a ++ '[' : (show x) ++ "]") "" e)
-    show (Func n p) = n ++ '(' : (tail . init . show) p ++ ")"
+import AST
 
 whitespace :: Parser ()
 whitespace = space *> spaces
@@ -89,6 +44,11 @@ expr = ( lbinop [">=",">","<=","<","!=","=="]  -- Menor precedência
        . lbinop ["*","/"]                      -- Maior precedência
        ) $ (between spaces spaces term <?> "expression")
 
+lbinop :: [String] -> Parser Expr -> Parser Expr
+lbinop ops unit = foldl (\e (a,b)->BinOp e a b) <$> unit
+                        <*> many ((\a b->(a,b)) <$> (oneOfStr ops <?> "operator")
+                                                <*> unit)
+
 alloc :: Parser Expr
 alloc = Alloc <$> (try (word "new") *> whitespace *> typename <* spaces)
               <*> many1 (between (char '[') (char ']' *> spaces) expr)
@@ -101,11 +61,6 @@ negation = Not <$> (char '!' *> spaces *> term)
 
 call :: Parser Expr
 call = Func <$> nameid <* spaces <*> between (char '(') (char ')') (expr `sepBy` (char ','))
-
-lbinop :: [String] -> Parser Expr -> Parser Expr
-lbinop ops unit = foldl (\e (a,b)->BinOp e a b) <$> unit
-                        <*> many ((\a b->(a,b)) <$> (oneOfStr ops <?> "operator")
-                                                <*> unit)
 
 term :: Parser Expr
 term = between (char '(') (char ')') expr
@@ -130,7 +85,7 @@ command = while
       <|> Expr <$> expr <* char ';'
 
 while :: Parser Command
-while = While <$> (try (word "while") *> spaces
+while = While <$> (try (word "enquanto") *> spaces
                *> between (char '(') (char ')') expr <* spaces)
               <*> block
 
@@ -149,7 +104,7 @@ attrib :: Parser Command
 attrib = Attrib <$> (var <* spaces <* string "<-") <*> expr
 
 free :: Parser Command
-free = Free <$> (try (word "free") *> whitespace *> nameid)
+free = Free <$> (try (word "free") *> whitespace *> expr)
 
 for :: Parser Command
 for =  For <$> (try (word "itere") *> spaces
