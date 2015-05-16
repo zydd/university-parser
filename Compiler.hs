0af4cb38ed92f4191812f5e45253e6454424ca7a
Gabriel Machado <gabriel_machado@live.com>
2015-05-15 23:45:51 -0300

    if and while opcodes

--- Compiler.hs
+++ Compiler.hs
@@ -1,21 +1,24 @@
-module Compiler where
+module Compiler(compile) where
 
 import AST
 import qualified StackMachine as S
 
-
-import qualified Parser as P
-import Text.Parsec.String
-
 class Stackable a where
     compile :: a -> S.Program
 
+instr "+" = S.Add
+instr "-" = S.Sub
+instr "*" = S.Mult
+instr "/" = S.Div
+instr "<" = S.Lt
+instr ">" = S.Gt
+instr "==" = S.Eq
+
 instance Stackable Expr where
-    compile (BinOp e1 "+" e2) = compile e1 ++ compile e2 ++ [S.Add]
-    compile (BinOp e1 "-" e2) = compile e1 ++ compile e2 ++ [S.Sub]
-    compile (BinOp e1 "*" e2) = compile e1 ++ compile e2 ++ [S.Mult]
-    compile (BinOp e1 "/" e2) = compile e1 ++ compile e2 ++ [S.Div]
+    compile (BinOp e1 op e2) = compile e1 ++ compile e2 ++ [instr op]
+    compile (Not e) = compile e ++ [S.Not]
     compile (IntLit x) = [S.Push $ S.Int x]
+    compile (BoolLit b) = [S.Push $ S.Bool b]
     compile (Var n []) = [S.Push $ S.Var n]
 
 instance Stackable Program where
@@ -25,6 +28,9 @@ instance Stackable Scope where
     compile (Scope _ cs) = concat $ map compile cs
 
 instance Stackable Command where
-    compile (Expr e) = compile e -- ++ [Pop]
+    compile (Expr e) = compile e -- ++ [S.Pop]
     compile (Attrib v e) = compile v ++ compile e ++ [S.Attr]
+    compile (If e cs Nothing) = [S.Push $ S.Block $ concat $ map compile cs] ++ compile e ++ [S.If]
+    compile (While e cs) = [S.Push (S.Block $ concat (map compile cs) ++ cond)] ++ cond ++ [S.While]
+        where cond = compile e
 
--- StackMachine.hs
+++ StackMachine.hs
@@ -2,19 +2,27 @@ module StackMachine where
 
 import qualified Data.Map.Strict as Map
 
-data Data = Int Int | String String | Bool Bool | Var String
+data Data = Int Int | String String | Bool Bool | Var String | Block [Instruction]
     deriving Show
-data Instruction = Push Data | Add | Sub | Mult | Div | Attr
+data Instruction = Push Data | Add | Sub | Mult | Div | Attr | Not | Lt | Gt | Eq | If | While
     deriving Show
 
 type Stack = [Data]
 type Program = [Instruction]
 type Memory = Map.Map String Data
 
-(Int x) |+| (Int y) = Int (x + y)
-(Int x) |-| (Int y) = Int (x - y)
-(Int x) |*| (Int y) = Int (x * y)
+instance Num Data where
+    (Int x) + (Int y) = Int (x + y)
+    (Int x) - (Int y) = Int (x - y)
+    (Int x) * (Int y) = Int (x * y)
+    abs (Int x) = Int (abs x)
+    signum (Int x) = Int (signum x)
+    fromInteger x = Int (fromInteger x)
+
 (Int x) |/| (Int y) = Int (x `div` y)
+(Int x) |>| (Int y) = Bool (x > y)
+(Int x) |<| (Int y) = Bool (x < y)
+(Int x) |==| (Int y) = Bool (x == y)
 
 deref :: Memory -> Data -> Data
 deref mem (Var x) = case Map.lookup x mem of Just x -> x
@@ -23,12 +31,24 @@ deref _ x = x
 exec :: (Stack,Memory) -> Program -> (Stack,Memory)
 exec (st, mem) (Push x : ins) = exec (x : st, mem) ins
 
-exec (y : x : st, mem) (Add  : ins) = exec (deref mem x |+| deref mem y : st, mem) ins
-exec (y : x : st, mem) (Sub  : ins) = exec (deref mem x |-| deref mem y : st, mem) ins
-exec (y : x : st, mem) (Mult : ins) = exec (deref mem x |*| deref mem y : st, mem) ins
+exec (y : x : st, mem) (Add  : ins) = exec (deref mem x  +  deref mem y : st, mem) ins
+exec (y : x : st, mem) (Sub  : ins) = exec (deref mem x  -  deref mem y : st, mem) ins
+exec (y : x : st, mem) (Mult : ins) = exec (deref mem x  *  deref mem y : st, mem) ins
 exec (y : x : st, mem) (Div  : ins) = exec (deref mem x |/| deref mem y : st, mem) ins
 
+exec (y : x : st, mem) (Lt   : ins) = exec (deref mem x |<|  deref mem y : st, mem) ins
+exec (y : x : st, mem) (Gt   : ins) = exec (deref mem x |>|  deref mem y : st, mem) ins
+exec (y : x : st, mem) (Eq   : ins) = exec (deref mem x |==| deref mem y : st, mem) ins
+
+exec (Bool x : st, mem) (Not : ins) = exec (Bool (not x) : st, mem) ins
+
 exec (y : Var x : st, mem) (Attr : ins) = exec (st, Map.insert x y mem) ins
+
+exec (Bool True  : Block blk : st, mem) (If : ins) = exec (exec (st,mem) blk) ins
+exec (Bool False : Block blk : st, mem) (If : ins) = exec (st,mem) ins
+
+exec (Bool True  : Block blk : st, mem) (While : ins) = exec (exec (Block blk : st,mem) blk) (While : ins)
+exec (Bool False : Block blk : st, mem) (While : ins) = exec (st,mem) ins
 
 exec sm [] = sm
 
